47ed280978f9c30ee9118fcd4b6043e21515b52e
Zimin Liang <zxl525@cs.bham.ac.uk>
2019-01-18 17:23:29 +0000

    test commit and push

--- activityapps/src/activityapps/Event.java
+++ activityapps/src/activityapps/Event.java
@@ -1,5 +1,45 @@
 package activityapps;
 
-public class Event {
+import java.util.ArrayList;
+import java.util.Date;
 
+public class Event {
+	private String name;
+	private Date date;
+	private Staff charge;
+	
+	private ArrayList<Item> eventItems;
+
+	public Event(String name, Date date, Staff charge) {
+		super();
+		this.name = name;
+		this.date = date;
+		this.charge = charge;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Staff getCharge() {
+		return charge;
+	}
+
+	public void setCharge(Staff charge) {
+		this.charge = charge;
+	}
+	
+	
 }
--- activityapps/src/activityapps/Staff.java
+++ activityapps/src/activityapps/Staff.java
@@ -1,5 +1,53 @@
 package activityapps;
 
-public class Staff {
+import java.util.ArrayList;
 
+public class Staff {
+	private String name;
+	private String gender;
+	private String department;
+	private String position;
+	private ArrayList<String> tasks;
+	
+	public Staff(String name, String gender, String department, String position) {
+		super();
+		this.name = name;
+		this.gender = gender;
+		this.department = department;
+		this.position = position;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	
+	
 }
